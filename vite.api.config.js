00059339baf024271d19d608e7ac22cbb125dbe2
Luka0308 <dagosluk@gmail.com>
2025-01-17 12:55:49 -0300

    Update vite.api.config.js

--- vite.api.config.js
+++ vite.api.config.js
@@ -1,13 +1,13 @@
-import { URL, fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
-import EsJS from '@es-js/vite-plugin-esjs'
-import devServer from '@hono/vite-dev-server'
+import { URL, fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import EsJS from '@es-js/vite-plugin-esjs';
+import devServer from '@hono/vite-dev-server';
 
 export default defineConfig({
   plugins: [
-    // https://github.com/es-js/esjs
+    // Configuración del plugin EsJS
     EsJS(),
-
+    // Configuración del servidor de desarrollo de Hono
     devServer({
       entry: './api/api.esjs',
     }),
@@ -15,23 +15,23 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./api', import.meta.url)),
+      '@': fileURLToPath(new URL('./api', import.meta.url)), // Alias para la carpeta `api`
     },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.esjs'],
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.esjs'], // Extensiones compatibles
   },
 
   build: {
-    outDir: './dist',
-    emptyOutDir: false,
+    outDir: './dist', // Carpeta de salida
+    emptyOutDir: true, // Limpia la carpeta antes de cada build
     rollupOptions: {
-      input: './api/api.esjs',
+      input: './api/api.esjs', // Archivo principal de entrada
       output: {
-        format: 'esm',
-        entryFileNames: 'servidor/[name].js',
-        chunkFileNames: 'servidor/[name].js',
-        assetFileNames: 'servidor/[name].[ext]',
-        exports: 'auto',
+        format: 'esm', // Formato del módulo de salida
+        entryFileNames: 'servidor/[name].js', // Nombre del archivo de entrada
+        chunkFileNames: 'servidor/[name].js', // Nombres de los chunks
+        assetFileNames: 'servidor/[name].[ext]', // Nombres de los archivos de recursos
+        exports: 'auto', // Exportación automática
       },
     },
   },
-})
+});
